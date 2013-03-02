e3c5552fdda0edb344ceb8d903672a9a91386265
xaicron <xaicron@gmail.com>
2013-03-02 19:48:40 +0900

    fixed pod

--- lib/Micro/Container.pm
+++ lib/Micro/Container.pm
@@ -175,6 +175,7 @@ Returns instance.
   my $container = MyContainer->instance;
 
 =head2 register(%args)
+
 =head2 add(%args)
 
 Register objects to container.
@@ -191,6 +192,7 @@ Register objects to container.
   );
 
 =head2 unregister(@names)
+
 =head2 remove(@names)
 
 Remove registered objects by name.
